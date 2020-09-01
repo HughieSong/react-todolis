8fad2074dc0dc9e3d34e74c02e38e0896636eda8
songhao <songhao@lvwan.com>
2020-09-01 12:49:04 +0800

    0901

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+// import { Component } from 'react'
+// // 等价于
+// import React from 'react';
+// const Component = React.Component;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       hello
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        hello111
+      </div>
+    )
+  }
+}
+
+
+
+export default App;
