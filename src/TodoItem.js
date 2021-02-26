60b9ca94116a060f72faeeb8df6fb117d4621b89
songhao <songhao@lvwan.com>
2021-02-26 18:10:48 +0800

    02-26

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 class TodoItem extends Component {
 
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);//bind(this)在这里节约性能
+    this.handleClick = this.handleClick.bind(this);// ! bind(this)在这里节约性能
   }
 
   render() {
-    const { content } = this.props;
-    return <div onClick={this.handleClick}>{content}</div>
+    const { content, test } = this.props;
+    return <div onClick={this.handleClick}>{test}-{content}</div>
   }
 
   handleClick() {
     const { deleteItem, index } = this.props;
     deleteItem(index)
   }
+}
+
+// 外部传来的参数进行类型校验
+TodoItem.propTypes = {
+  test: PropTypes.string.isRequired,// ! isRequired表示必传
+  content: PropTypes.arrayOf(PropTypes.number, PropTypes.string),
+  deleteItem: PropTypes.func,
+  index: PropTypes.number
+};
+// 定义默认值
+TodoItem.defaultProps = {
+  test: 'hello world'
 }
 
 export default TodoItem;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,13 +1,13 @@
-import React, { Component, Fragment } from 'react';// Fragment 占位符 组件必须只有一个根标签，h5标签作为根标签会被渲染出来，用占位符做根标签不会被渲染
+import React, { Component, Fragment } from 'react';// ! Fragment 占位符 组件必须只有一个根标签，h5标签作为根标签会被渲染出来，用占位符做根标签不会被渲染
 import TodoItem from './TodoItem';
 import './style.css';
 
 class TodoList extends Component {
-  // constructor()：构造方法。在js里面一个class一定有一个constructor()构造函数,使用组件的时候constructor()最优先被执行,constructor接收一个参数props
+  // ! constructor()：构造方法。在js里面一个class一定有一个constructor()构造函数,使用组件的时候constructor()最优先被执行,constructor接收一个参数props
   constructor(props) {
-    // super()：继承。在class方法中，继承是使用 extends 关键字来实现的。子类 必须 在 constructor()调用 super()方法，否则新建实例时会报错。
+    // ! super()：继承。在class方法中，继承是使用 extends 关键字来实现的。子类 必须 在 constructor()调用 super()方法，否则新建实例时会报错。
     super(props);
-    // this.state是组件的状态,负责存储组件的数据
+    // ! this.state是组件的状态,负责存储组件的数据
     this.state = {
       inputValue: '',
       list: []
@@ -16,83 +16,102 @@ class TodoList extends Component {
     this.handleBtnClick = this.handleBtnClick.bind(this)
     this.handleItemDelete = this.handleItemDelete.bind(this)
   }
-    render() {
-      return (
-        <Fragment>
-          <div>
-            {/* 多行注释 */}
-            {
-              // 单行注释
-            }
-            {/* 1、表达式要通过花括号进行包裹
+  render() {
+    return (
+      <Fragment>
+        <div>
+          {/* 多行注释 */}
+          {
+            // 单行注释
+          }
+          {/* 1、表达式要通过花括号进行包裹
               2、原生的绑定事件onchange小写，react的绑定事件onChange大写
               3、事件绑定的时候要通过bind(this)对函数作用域进行变更
           */}
-            {/* className代替class */}
-            <label htmlFor="insertArea">输入内容</label>
-            <input
-              id='insertArea'
-              className='input'
-              value={this.state.inputValue}
-              // onChange={this.handleInputChange.bind(this)}
-              onChange={this.handleInputChange}
-            />
+          {/* className代替class */}
+          <label htmlFor="insertArea">输入内容</label>
+          <input
+            id='insertArea'
+            className='input'
+            value={this.state.inputValue}
+            // onChange={this.handleInputChange.bind(this)}
+            onChange={this.handleInputChange}
+          />
 
-            <button onClick={this.handleBtnClick}>提交</button>
-          </div>
-          <ul>
-            {
-              this.state.list.map((item, index) => {
-                return (
-                  <div key={index}>
-                    <TodoItem
-                      // 通过属性既可以传值又可以传方法
-                      content={item}
-                      index={index}
-                      deleteItem={this.handleItemDelete}
-                    />
-                    {/*<li
-                  key={index}
-                  // 传递参数放在this后面
-                  onClick={this.handleItemDelete.bind(this, index)}
-                  // dangerouslySetInnerHTML={{__html: item}}  dangerouslySetInnerHTML表示不转义在页面上显示的内容，使用时标签内text应为空
-                  dangerouslySetInnerHTML={{ __html: item }}
-                >
-                </li>*/}
-                  </div>
-                )
-              })
-            }
-          </ul>
-        </Fragment>
-      )
-    }
-
-    handleInputChange(e) {
-      // console.log(e.target.value)
-      // console.log(this)
-      // 注意this的指向问题，bind(this)这里的bind将this的作用域指定到组件实例
-      this.setState({
-        inputValue: e.target.value
-      })
-    }
-    handleBtnClick() {
-      this.setState({
-        // ...es6展开运算符
-        list: [...this.state.list, this.state.inputValue],
-        inputValue: ''
-      })
-    }
-    handleItemDelete(index) {
-      // immutable
-      // state 不允许我们做任何的改变
-      const list = [...this.state.list];
-      list.splice(index, 1);
-      this.setState({
-        list: list
-      })
-    }
+          <button onClick={this.handleBtnClick}>提交</button>
+        </div>
+        <ul>
+          {this.getTodoItem()}
+        </ul>
+      </Fragment>
+    )
   }
 
+  getTodoItem() {
+    return this.state.list.map((item, index) => {
+      return (
+        <div key={index}>
+          <TodoItem
+            // 通过属性既可以传值又可以传方法
+            content={item}
+            index={index}
+            deleteItem={this.handleItemDelete}
+          />
+          {/*<li
+          key={index}
+          // 传递参数放在this后面
+          onClick={this.handleItemDelete.bind(this, index)}
+          // dangerouslySetInnerHTML={{__html: item}}  dangerouslySetInnerHTML表示不转义在页面上显示的内容，使用时标签内text应为空
+          dangerouslySetInnerHTML={{ __html: item }}
+        >
+        </li>*/}
+        </div>
+      )
+    })
+  }
 
-  export default TodoList;
+  handleInputChange(e) {
+    // console.log(e.target.value)
+    // console.log(this)
+    // ! 注意this的指向问题，bind(this)这里的bind将this的作用域指定到组件实例
+    // this.setState({
+    //   inputValue: e.target.value
+    // })
+    // TODO: 新版本写法
+    const value = e.target.value;
+    this.setState(() => ({
+      inputValue: value
+    }))
+  }
+  handleBtnClick() {
+    // this.setState({
+    //   list: [...this.state.list, this.state.inputValue],
+    //   inputValue: ''
+    // })
+
+    this.setState((prevState) => ({
+      list: [...prevState.list, prevState.inputValue],
+      inputValue: ''
+    }));
+  }
+  handleItemDelete(index) {
+    // immutable
+    // state 不允许我们做任何的改变
+    // const list = [...this.state.list];
+    // list.splice(index, 1);
+    // this.setState({
+    //   list: list
+    // })
+
+    this.setState((prevState) => {
+      const list = [...prevState.list];
+      list.splice(index, 1)
+      return {
+        list
+      }
+    })
+  }
+}
+
+
+export default TodoList;
