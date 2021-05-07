c1f5bca157225e86059ffdc4802f3b71c0031e7a
HughieSung <sh9830@outlook.com>
2021-05-07 22:19:17 +0800

    actionCreator

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
 import store from './store';
-import { CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM } from './store/actionTypes';//拆分actionTypes便于排错
+import { getInputChangeAction, getAddItemAction, getDeleteItemAction } from './store/actionCreator';//使用actionCreator统一创建action
+// import { CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM } from './store/actionTypes';//拆分actionTypes便于排错
 class TodoList extends Component {
   constructor(props) {
     super(props);
@@ -34,11 +35,12 @@ class TodoList extends Component {
     )
   }
   handleInputChange(e) {
-    const action = {
-      // type: 'change_input_value',
-      type: CHANGE_INPUT_VALUE,
-      value: e.target.value
-    }
+    // const action = {
+    //   // type: 'change_input_value',
+    //   type: CHANGE_INPUT_VALUE,
+    //   value: e.target.value
+    // }
+    const action = getInputChangeAction(e.target.value);
     store.dispatch(action);
   }
   handleStoreChange() {
@@ -46,19 +48,21 @@ class TodoList extends Component {
     this.setState(store.getState())
   }
   handleBtnClick() {
-    const action = {
-      // type: 'add_todo_item'
-      type: ADD_TODO_ITEM
-    };
+    // const action = {
+    //   // type: 'add_todo_item'
+    //   type: ADD_TODO_ITEM
+    // };
+    const action = getAddItemAction();
     store.dispatch(action)
   }
   handleItemDelete(index) {
     console.log(index)
-    const action = {
-      // type: 'delete_todo_item',
-      type: DELETE_TODO_ITEM,
-      index
-    }
+    // const action = {
+    //   // type: 'delete_todo_item',
+    //   type: DELETE_TODO_ITEM,
+    //   index
+    // }
+    const action = getDeleteItemAction(index);
     store.dispatch(action)
   }
 }
