7b9deedfa545adc7547d976a82987915a4402dfd
songhao <songhao@lvwan.com>
2021-05-08 17:47:58 +0800

    react-redux

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,52 +1,24 @@
 import React, { Component } from 'react';
-import 'antd/dist/antd.css';
 import store from './store';
-import { getInitList, getInputChangeAction, getAddItemAction, getDeleteItemAction } from './store/actionCreator';//使用actionCreator统一创建action
-import TodoListUI from './TodoListUI';
-
 class TodoList extends Component {
+
   constructor(props) {
     super(props);
     this.state = store.getState();
-
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleStoreChange = this.handleStoreChange.bind(this);
-    this.handleBtnClick = this.handleBtnClick.bind(this);
-    this.handleItemDelete = this.handleItemDelete.bind(this);
-    // 订阅redux更新
-    store.subscribe(this.handleStoreChange);
   }
+
   render() {
     return (
-      <TodoListUI
-        inputValue={this.state.inputValue}
-        list={this.state.list}
-        handleInputChange={this.handleInputChange}
-        handleBtnClick={this.handleBtnClick}
-        handleItemDelete={this.handleItemDelete}
-      />
+      <div>
+        <div>
+          <input value={this.state.inputValue} />
+          <button>提交</button>
+        </div>
+        <ul>
+          <li>test</li>
+        </ul>
+      </div>
     )
-  }
-
-  componentDidMount() {
-    const action = getInitList();
-    store.dispatch(action);
-  }
-
-  handleInputChange(e) {
-    const action = getInputChangeAction(e.target.value);
-    store.dispatch(action);
-  }
-  handleStoreChange() {
-    this.setState(store.getState())
-  }
-  handleBtnClick() {
-    const action = getAddItemAction();
-    store.dispatch(action)
-  }
-  handleItemDelete(index) {
-    const action = getDeleteItemAction(index);
-    store.dispatch(action)
   }
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,6 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore } from 'redux';
 import reducer from './reducer';
-import createSagaMiddleware from 'redux-saga';// https://github.com/redux-saga/redux-saga
-import todoSagas from './sagas';
 
+const store = createStore(reducer);
 
-const sagaMiddleware = createSagaMiddleware();
-const composeEnhancers =window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
-const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware));
-const store = createStore(reducer, enhancer);
-sagaMiddleware.run(todoSagas);
 export default store;
