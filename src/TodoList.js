ed40a0bf777028746d3d85c67ebe3fd669f53bed
songhao <songhao@lvwan.com>
2021-05-11 19:27:00 +0800

    todolist

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,34 +1,47 @@
 import React, { Component } from 'react';
-import store from './store';
 import { connect } from 'react-redux';
-class TodoList extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = store.getState();
-  }
-
+// 抽离为无状态组件
+const TodoList = (props) => {
+  const { inputValue, list, changeInputValue, handleClick, handleDelete } = props;
+  return (
+    <div>
+      <div>
+        <input value={inputValue} onChange={changeInputValue} />
+        <button onClick={handleClick}>提交</button>
+      </div>
+      <ul>
+        {list.map((item, index) => {
+          return <li onClick={handleDelete.bind(this, index)} key={index}>{item}</li>
+        })}
+      </ul>
+    </div>
+  )
+}
+/* class TodoList extends Component {
   render() {
+    const { inputValue, list, changeInputValue, handleClick, handleDelete } = this.props;
     return (
       <div>
         <div>
-          <input value={this.props.inputValue} onChange={this.props.changeInputValue} />
-          <button onClick={this.props.handleBtnClick}>提交</button>
+          <input value={inputValue} onChange={changeInputValue} />
+          <button onClick={handleClick}>提交</button>
         </div>
         <ul>
-          <li>test</li>
+          {list.map((item, index) => {
+            return <li onClick={handleDelete.bind(this, index)} key={index}>{item}</li>
+          })}
         </ul>
       </div>
     )
   }
-
-  handleBtnClick() { }
-}
+} */
 
 // store里面的公用数据state映射到组件里的props
 const mapStateToProps = (state) => {
   return {
-    inputValue: state.inputValue
+    inputValue: state.inputValue,
+    list: state.list
   }
 }
 // store.dispatch方法挂载到props上
@@ -41,9 +54,23 @@ const mapDispatchToProps = (dispatch) => {
       }
       dispatch(action);
       console.log(e.target.value)
+    },
+    handleClick() {
+      const action = {
+        type: 'add_item'
+      }
+      dispatch(action);
+    },
+    handleDelete(index) {
+      console.log(index)
+      const action = {
+        type: 'delete_item',
+        index
+      }
+      dispatch(action);
     }
   }
 }
 
-// TodoList和store作连接
+// TodoList和store作连接   connect返回容器组件（mapStateToProps和mapDispatchToProps是业务逻辑，TodoList是UI组件）
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
